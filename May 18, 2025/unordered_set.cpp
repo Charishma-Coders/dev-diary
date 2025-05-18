bc4ebdff5b262fd23376916da91ac65bfb6fec72
Charishma <charishma.coder@gmail.com>
2025-05-18 11:54:02 +0530

    Learned unorderd_set in C++ STL

---
+++ May 18, 2025/unordered_set.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+int main(){
+    // Random order - Not sorted
+    // Unique
+    
+    unordered_set<int> un_set;
+    
+    // most of the cases time complexity is O(1) - erase, insert
+    // worst case - O(n)
+    
+    return 0;
+}
